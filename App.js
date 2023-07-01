c3407d568adab9261b41b48a49d302b9149934ee
RanjithRajamudi <ranjithrajmudigowda@gmail.com>
2023-07-01 15:29:30 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,4 +1,26 @@
-const heading1 = React.createElement("h1", { id: "title" }, "Heading 1");
+/**
+ * HMR- Hot module replacement
+ * File Watcher Algorithm - written in C++
+ * Bundling
+ * Minification
+ * Cleaning our code   
+ * Dev and production build
+ * Super fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compressiion
+ * Compatibile with older version of browser
+ * HTTPS on development
+ * Manages port number 
+ * Consistent Hashing Algorithm
+ * Zero Config
+ * Transitive Dependencies
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading1 = React.createElement("h1", { id: "title" }, "Heading 1 from parcel");
 
 const heading2 = React.createElement("h2", { id: "title" }, "Heading 2");
 
