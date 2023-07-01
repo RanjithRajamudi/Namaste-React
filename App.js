f03e7b57133e8c189ff57c02c9f82ea0b05474e1
RanjithRajamudi <ranjithrajmudigowda@gmail.com>
2023-07-01 19:53:11 +0530

    Episode-03

--- App.js
+++ App.js
@@ -1,32 +1,23 @@
-/**
- * HMR- Hot module replacement
- * File Watcher Algorithm - written in C++
- * Bundling
- * Minification
- * Cleaning our code   
- * Dev and production build
- * Super fast build algorithm
- * Image Optimization
- * Caching while development
- * Compressiion
- * Compatibile with older version of browser
- * HTTPS on development
- * Manages port number 
- * Consistent Hashing Algorithm
- * Zero Config
- * Transitive Dependencies
- */
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading1 = React.createElement("h1", { id: "title" }, "Heading 1 from parcel");
+const Title = () => (
+    <h1 id="title" key="h1">
+        Namaste React
+    </h1>
+);
 
-const heading2 = React.createElement("h2", { id: "title" }, "Heading 2");
+// Composing Components
 
-const container = React.createElement("div", { id: "container" }, [heading1, heading2]);
+const HeaderComponent = () => (
+    <div>
+        <Title />
+        <h1>Namaste React functional Component</h1>
+        <h2>This is a H2 tag</h2>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// passing a react element inside the root
-root.render(container);
+//async defer
+root.render(<HeaderComponent />);
